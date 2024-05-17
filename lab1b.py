73da2eb7793cba09ec25d249bc999cb6aaba9ab1
Andres Wong <agwong-lam@myseneca.ca>
2024-05-17 19:59:03 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+name = 'Isaac'
+# print(name)
+# this is going to print the name thomas because it is what the variable is set to
+# print('name')
+# this is going to just print "name" because what we set there was just a string =)
+print('How old are you ' + name + '?')
+# this going to print "How old are you" with a space in the end due to it being the strings plus the variable
+# we set at the top for the name that is Isaac
