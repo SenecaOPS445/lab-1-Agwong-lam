fd9bbe5a5429db6b2daf0ae372e44320ff7237cb
Andres Wong <agwong-lam@myseneca.ca>
2024-05-17 20:12:02 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+name = 'Isaac'
+age = 72
+# num1 = 5
+# num2 = 10
+
+# print(num1)
+# print(num2)
+
+# sum = num1 + num2
+
+# print(sum)
+
+# print('The sum is: ' + sum)
+# print('The sum is: ' + str(sum))
+
+print(name + ' is ' + str(age) + ' years old!')
+
